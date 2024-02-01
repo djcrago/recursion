86178a294d3bc4268664a652833ad246043526e8
Dexter Crago <jodieanddexter@gmail.com>
2024-02-01 10:21:30 -0700

    Fibonacci

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
 import './style.css';
+
+function fibs(n) {
+  const array = [0, 1];
+
+  for (let i = 1; i <= n - 2; i += 1) {
+    const sum = array[i] + array[i - 1];
+    array.push(sum);
+  }
+
+  return array;
+}
+
+console.log(fibs(8));
+
+function fibsRec(n) {
+  if (n === 1) {
+    return [0];
+  }
+
+  if (n === 2) {
+    return [0, 1];
+  }
+
+  const array = fibsRec(n - 1);
+
+  const previous = array[array.length - 2];
+  const current = array[array.length - 1];
+  array.push(previous + current);
+
+  return array;
+}
+
+console.log(fibsRec(8));
